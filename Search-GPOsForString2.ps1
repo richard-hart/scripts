230c98ff9581482f4d546f89aaa3d382a145cd8d
Richard Hart <44090547+richard-hart@users.noreply.github.com>
2019-01-30 09:03:04 +0000

    Add files via upload

---
+++ Search-GPOsForString2.ps1
@@ -0,0 +1,24 @@
+$String = "foobar"
+$Domain = "acme.com"
+
+$NearestDC = (Get-ADDomainController -Discover -NextClosestSite).Name
+
+#Get a list of GPOs from the domain
+$GPOs = Get-GPO -All -Domain $Domain | sort DisplayName
+
+#Go through each Object and check its XML against $String
+Foreach ($GPO in $GPOs)  {
+  
+  Write-Host "Working on $($GPO.DisplayName)"
+  
+  #Get Current GPO Report (XML)
+  $CurrentGPOReport = Get-GPOReport -Guid $GPO.Id -ReportType Xml -Domain $Domain
+  
+  If ($CurrentGPOReport -match $String)  {
+	Write-Host "A Group Policy matching ""$($String)"" has been found:" -Foregroundcolor Green
+	Write-Host "-  GPO Name: $($GPO.DisplayName)" -Foregroundcolor Green
+	Write-Host "-  GPO Id: $($GPO.Id)" -Foregroundcolor Green
+	Write-Host "-  GPO Status: $($GPO.GpoStatus)" -Foregroundcolor Green
+  }
+  
+}
